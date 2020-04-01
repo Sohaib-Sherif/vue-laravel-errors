61b4f22b6865a95f5ae503182bfeb18a1a00543e
Sohaib Sherif <s.alsharif@tech.arttech.ly>
2020-04-02 01:01:45 +0200

    fix errors

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ export default {
         Vue.mixin({
             data() {
                 return {
-                    errors : null,
+                    errors : {},
                 }
             },
             methods: {
