f392cf28d6b1b25b97c0d1d33b28217baf671d4e
Sohaib Sherif <s.alsharif@tech.arttech.ly>
2020-04-03 01:14:55 +0200

    added possibility to use mixin locally
    instead of only allowing it to be installed globally using a plugin
    now it can also be used locally in component

--- src/index.js
+++ src/index.js
@@ -1,38 +1,8 @@
+import {vueLaravelErrors} from 'mixin'
+
 export default {
     install(Vue, options) {
-
-        Vue.mixin({
-            data() {
-                return {
-                    errors : {},
-                }
-            },
-            methods: {
-                fillErrors(errors) {
-                    if(options.type === 'array') {
-                        errors = Object.entries(errors);
-                        errors = errors.flatMap(error => {
-                            return [...error[1]];
-                        })
-                    }
-                    else if(options.type === 'object') {
-                        errors = Object.entries(errors);
-                        errors = errors.map(error => {
-                            let key = error[0];
-                            let value = error[1][0];
-                            return {
-                                [key]: value
-                            };
-                        })
-                        .reduce((acc, current) => {
-                            acc = Object.assign(acc, current);
-                            return acc;
-                        });
-                    } 
-                    this.errors = errors;
-                }
-            },
-          })
+        Vue.mixin(vueLaravelErrors(options))
 
        }
    };
---
+++ src/vueLaravelErrors.js
@@ -0,0 +1,33 @@
+export default (type) => {
+    return {
+        data() {
+            return {
+                errors: {},
+            }
+        },
+        methods: {
+            fillErrors(errors) {
+                if (type === 'array') {
+                    errors = Object.entries(errors);
+                    errors = errors.flatMap(error => {
+                        return [...error[1]];
+                    })
+                } else if (type === 'object') {
+                    errors = Object.entries(errors);
+                    errors = errors.map(error => {
+                            let key = error[0];
+                            let value = error[1][0];
+                            return {
+                                [key]: value
+                            };
+                        })
+                        .reduce((acc, current) => {
+                            acc = Object.assign(acc, current);
+                            return acc;
+                        });
+                }
+                this.errors = errors;
+            }
+        },
+    }
+}
